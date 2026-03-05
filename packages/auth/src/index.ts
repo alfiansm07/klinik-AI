80b8ce52e1c505e32de205a80315cf4c3e97f41e
WAHYU RAMADHAN KALOKO <34205204+alfiansm07@users.noreply.github.com>
2026-03-05 21:57:22 +0700

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@klinik-AI/db";
+import * as schema from "@klinik-AI/db/schema/auth";
+import { env } from "@klinik-AI/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
